7dc83895f3c6d473cd1a10f7b238ae9a0e328aca
Enzo Henrique <ehenriique07@gmail.com>
2022-08-23 21:09:32 -0300

    update
    implementação com javascript na parte de login

--- js/script.js
+++ js/script.js
@@ -17,6 +17,19 @@ function trocarImage03() {
 
 
 
+////// pagina de login validação ////
+function logar(){
 
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+
+    if(login == "admin" && senha == "admin"){
+        alert('Sucesso');
+        
+    }else{
+        alert('Usuario ou senha incorretos');
+    }
+
+}
 
 
