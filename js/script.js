9e1f4354141f8c524c105bed74c504c9e9720d95
Enzo Henrique <ehenriique07@gmail.com>
2022-09-09 22:46:42 -0300

    update
    implementacao de jquery e javascript funcionando

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
 //// troca icon por imagem da home ////
 
 function trocarImage01() {
-    document.getElementById("img01").src="/img/monitor.jpg";
+    document.getElementById("img01").src="img/monitor.jpg";
 } 
 
-$("trocarImage02").attr("src","/img/iconmonitor.jpg");
 
 function trocarImage02() {
-    document.getElementById("img02").src="/img/nintendo.jpg";
+    document.getElementById("img02").src="img/nintendo.jpg";
 }
 
 function trocarImage03() {
-    document.getElementById("img03").src="/img/pcgamer.jpg";
+    document.getElementById("img03").src="img/pcgamer.jpg";
 }
     
 
